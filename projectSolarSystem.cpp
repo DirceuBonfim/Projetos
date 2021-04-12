91fd76426254d255a4481dd7b2d37bc25c70f6da
DirceuBonfim <dirceubonfim@yahoo.com.br>
2021-04-11 23:17:37 -0300

    up2
    há vários erros nos arquivos para corrigir  estou quebrando em padaços para descobrir o erro

--- projectSolarSystem.cpp
+++ projectSolarSystem.cpp
@@ -4,17 +4,16 @@ using std::string;
 class Planeta{
 public:
     string Nome;//do planeta
-    double Rotacao;//distância média do sol
-    double Translacao;
-    double Diametro;
-    long Massa;
-    double TempMedia;//temteratura média na superficie
-    double PresAtmosferica;//Pressão atmosférica
+    float Rotacao;//distância média do sol
+    float Translacao;
+    float Diametro;
+    float Massa;
+    float TempMedia;//temteratura média na superficie
+    float PresAtmosferica;//Pressão atmosférica
     string Luas;
     string Composicao;//do que é feito
 
     void mostrarPlanetas(){
-        std::cout.precision(5);
         std::cout<<"Nome "<< Nome <<std::endl;
         std::cout<<"Rotação "<< Rotacao <<std::endl;
         std::cout<<"Translacao "<< Translacao <<std::endl;
@@ -25,32 +24,43 @@ public:
         std::cout<<"Luas "<< Luas <<std::endl;
         std::cout<<"Composicao "<< Composicao <<std::endl;
         }
+        Planeta(string nome, float rotacao, float translacao, float diametro, float massa, float tempMedia, float presAtmosferica, string luas, string composicao){
+            Nome = nome;
+            Rotacao = rotacao;
+            Translacao = translacao;
+            Diametro = diametro;
+            Massa = massa;
+            TempMedia = tempMedia;
+            PresAtmosferica = presAtmosferica;
+            Luas = luas;
+            Composicao = composicao;
+        }
 };
 
 int main()
 {
-    Planeta planeta1;
-    planeta1.Nome = "Mercúrio";
-    planeta1.Rotacao = 58 ;//horas
-    planeta1.Translacao = 87;//dias
-    planeta1.Diametro = 4878;//em km
-    planeta1.Massa = 3,303*1023;//em kg
-    planeta1.TempMedia = 127;//em graus celsius
-    planeta1.PresAtmosferica = 92; //em Bar
-    planeta1.Luas = "nenhuma";
-    planeta1.Composicao = "Hélio, sódio, oxigênio";
+    Planeta planeta1 = Planeta("Mercúrio", 58, 87, 4878,3,303*1023, 127, 92, "nenhuma", "Hélio, sódio, oxigênio" );
+    //planeta1.Nome = "Mercúrio";
+    //planeta1.Rotacao = 58 ;//horas
+    //planeta1.Translacao = 87;//dias
+    //planeta1.Diametro = 4878;//em km
+    //planeta1.Massa = 3,303*1023;//em kg
+    //planeta1.TempMedia = 127;//em graus celsius
+    //planeta1.PresAtmosferica = 92; //em Bar
+    //planeta1.Luas = "nenhuma";
+    //planeta1.Composicao = "Hélio, sódio, oxigênio";
     planeta1.mostrarPlanetas();
 
-    Planeta planeta2;
-    planeta2.Nome = "Vênus";
-    planeta2.Rotacao = 243 ;//horas
-    planeta2.Translacao = 224;//dias
-    planeta2.Diametro = 12.102;//em km
-    planeta2.Massa = 4,8690 * 1024;//em kg
-    planeta2.TempMedia = 482;//em graus celsius
-    planeta2.PresAtmosferica = 92; //em Bar
-    planeta2.Luas = "nenhuma";
-    planeta2.Composicao = "Dióxido de Carbono, Nitrogênio";
+    Planeta planeta2 = Planeta();
+    //planeta2.Nome = "Vênus";
+    //planeta2.Rotacao = 243 ;//horas
+    //planeta2.Translacao = 224;//dias
+    //planeta2.Diametro = 12.102;//em km
+    //planeta2.Massa = 4,8690 * 1024;//em kg
+    //planeta2.TempMedia = 482;//em graus celsius
+    //planeta2.PresAtmosferica = 92; //em Bar
+    //planeta2.Luas = "nenhuma";
+    //planeta2.Composicao = "Dióxido de Carbono, Nitrogênio";
     planeta2.mostrarPlanetas();
 
 }
---
+++ projectSolarSystemTeste.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using std::string;
+
+class Planeta{
+public:
+    string Nome;//do planeta
+    string Luas;//distância média do sol
+    
+
+    void mostrarPlanetas(){
+        std::cout<<"Nome "<< Nome <<std::endl;
+        std::cout<<"Luas "<< Luas <<std::endl;
+        }
+        Planeta(int nome, int luas){
+            Nome = nome;
+            Luas = luas;
+            
+        }
+};
+
+int main()
+{
+    Planeta planeta1 = Planeta("Mercurio", "nenhuma");
+    //planeta1.Nome = "Mercurio"
+    //planeta1.Rotacao = 58 ;//horas
+    //planeta1.Translacao = 87;//dias
+    //planeta1.Diametro = 4878;//em km
+    //planeta1.Massa = 3,303*1023;//em kg
+    //planeta1.TempMedia = 127;//em graus celsius
+    //planeta1.PresAtmosferica = 92; //em Bar
+    //planeta1.Luas = "nenhuma";
+    //planeta1.Composicao = "Hélio, sódio, oxigênio";
+    planeta1.mostrarPlanetas();
+
+    Planeta planeta2 = Planeta("Venus", "Uma");
+    //planeta2.Nome = "Vênus";
+    //planeta2.Rotacao = 243 ;//horas
+    //planeta2.Translacao = 224;//dias
+    //planeta2.Diametro = 12.102;//em km
+    //planeta2.Massa = 4,8690 * 1024;//em kg
+    //planeta2.TempMedia = 482;//em graus celsius
+    //planeta2.PresAtmosferica = 92; //em Bar
+    //planeta2.Luas = "nenhuma";
+    //planeta2.Composicao = "Dióxido de Carbono, Nitrogênio";
+    planeta2.mostrarPlanetas();
+
+}
