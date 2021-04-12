8878be46c09284703320631ca53d56d7cba0e245
DirceuBonfim <dirceubonfim@yahoo.com.br>
2021-04-12 16:32:18 -0300

    Adição de mais caracteristicas
    Adição de mais características e correção de problemas

--- projectSolarSystemTeste.cpp
+++ projectSolarSystemTeste.cpp
@@ -5,21 +5,23 @@ class Planeta{
 public:
     string Nome;//do planeta
     string Luas;//distância média do sol
-    
+    string Rotacao;
 
     void mostrarPlanetas(){
-        std::cout<<"Nome "<< Nome <<std::endl;
-        std::cout<<"Luas "<< Luas <<std::endl;
+        std::cout<<"Nome - "<< Nome <<std::endl;
+        std::cout<<"Luas - "<< Luas <<std::endl;
+        std::cout<<"Rotacao - "<< Rotacao <<std::endl;
         }
-        Planeta(string nome, string luas){
+        Planeta(string nome, string luas, string rotacao){
             Nome = nome;
             Luas = luas;
-        }        
+            Rotacao = rotacao; 
+            }        
 };
 
 int main()
 {
-    Planeta planeta1 = Planeta("Mercurio", "Nenhuma");
+    Planeta planeta1 = Planeta("Mercurio","58","Nenhuma");
     //planeta1.Nome = "Mercurio";
     //planeta1.Rotacao = 58 ;//horas
     //planeta1.Translacao = 87;//dias
@@ -27,11 +29,11 @@ int main()
     //planeta1.Massa = 3,303*1023;//em kg
     //planeta1.TempMedia = 127;//em graus celsius
     //planeta1.PresAtmosferica = 92; //em Bar
-    planeta1.Luas = "nenhuma";
+    //planeta1.Luas = "nenhuma";
     //planeta1.Composicao = "Hélio, sódio, oxigênio";
     planeta1.mostrarPlanetas();
 
-    Planeta planeta2 = Planeta("Venus", "Nenhuma");
+    Planeta planeta2 = Planeta("Venus", "243", "Nenhuma");
     //planeta2.Nome = "Vênus";
     //planeta2.Rotacao = 243 ;//horas
     //planeta2.Translacao = 224;//dias
@@ -39,7 +41,7 @@ int main()
     //planeta2.Massa = 4,8690 * 1024;//em kg
     //planeta2.TempMedia = 482;//em graus celsius
     //planeta2.PresAtmosferica = 92; //em Bar
-    planeta2.Luas = "nenhuma";
+    //planeta2.Luas = "nenhuma";
     //planeta2.Composicao = "Dióxido de Carbono, Nitrogênio";
     planeta2.mostrarPlanetas();
 
